a9ccf7f15421a1fa0e30653703e62794233b8952
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2021-12-02 14:58:18 -0400

    🎄 01 (part 2)

--- 01/main.ts
+++ 01/main.ts
@@ -1,6 +1,6 @@
 import * as path from "https://deno.land/std@0.116.0/path/mod.ts";
 
-import { Totals } from "./types.ts";
+import { Result, Totals } from "./types.ts";
 
 const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 
@@ -10,52 +10,54 @@ const getReadings = async (filename: string) => {
   return decoder.decode(buffer).split("\n").map((line) => parseInt(line));
 };
 
+const calculate = (readings: number[]): Result[] => {
+  return readings.map(
+    (_, i, arr) => ({
+      reading: arr[i],
+      differential: arr[i] - arr[i - 1],
+      sum3: arr[i] + arr[i - 1] + arr[i - 2],
+    }),
+  ).map((result, i, arr) => ({
+    ...result,
+    sum3Differential: result.sum3 - arr[i - 1]?.sum3,
+  }));
+};
+
 const calculateTotals = (
-  readings: number[],
-  differentials: number[],
+  results: Result[],
 ): Totals => {
   return {
-    count: readings.length,
-    increased: differentials.reduce(
+    count: results.length,
+    diffInc: results.map((x) => x.differential).reduce(
       (count, x) => x > 0 ? count + 1 : count,
       0,
     ),
-    decreased: differentials.reduce(
+    diffDec: results.map((x) => x.differential).reduce(
+      (count, x) => x < 0 ? count + 1 : count,
+      0,
+    ),
+    sum3Inc: results.map((x) => x.sum3Differential).reduce(
+      (count, x) => x > 0 ? count + 1 : count,
+      0,
+    ),
+    sum3Dec: results.map((x) => x.sum3Differential).reduce(
       (count, x) => x < 0 ? count + 1 : count,
       0,
     ),
   };
 };
 
-const label = (i: number) => {
-  if (i > 0) {
-    return "increased";
-  }
-  if (i < 0) {
-    return "decreased";
-  }
-  return "---------";
-};
-
-const formatReading = (reading: number, differential: number) => {
-  return `${reading} (${label(differential)})`;
-};
-
-const formatReadings = (readings: number[], differentials: number[]) => {
-  return readings
-    .map((x, i) => formatReading(x, differentials[i]))
-    .join("\n");
+const formatResult = (result: Result) => {
+  return `${result.reading} (${result.differential}) (${result.sum3}) (${result.sum3Differential})`;
 };
 
 const main = async (args: string[]) => {
   const [filename = "readings"] = args;
   const readings = await getReadings(filename);
-  const differentials = readings.map(
-    (_, i, arr) => arr[i] - arr[i - 1],
-  );
-  const totals = calculateTotals(readings, differentials);
+  const results = calculate(readings);
+  const totals = calculateTotals(results);
 
-  console.log(formatReadings(readings, differentials));
+  console.log(results.map(formatResult).join("\n"));
   console.log();
   console.log(
     Object.entries(totals).map(([key, val]) => `${key}: ${val}`).join("\n"),
--- 01/types.ts
+++ 01/types.ts
@@ -1,5 +1,14 @@
+export interface Result {
+  reading: number;
+  differential: number;
+  sum3: number;
+  sum3Differential: number;
+}
+
 export interface Totals {
   count: number;
-  increased: number;
-  decreased: number;
+  diffInc: number;
+  diffDec: number;
+  sum3Inc: number;
+  sum3Dec: number;
 }
